b4ec080082e84385c6a974037734c33769361e10
Florian Chlan <florianchlan@punktorg.org>
2014-05-16 12:55:38 +0200

    [ticket #30935] EmployeeService für LoginSperre implementiert

--- tl_server/src/main/java/at/ac/tuwien/inso/tl/server/service/impl/EmployeeServiceImpl.java
+++ tl_server/src/main/java/at/ac/tuwien/inso/tl/server/service/impl/EmployeeServiceImpl.java
@@ -1,14 +1,23 @@
 package at.ac.tuwien.inso.tl.server.service.impl;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import at.ac.tuwien.inso.tl.dao.EmployeeDao;
 import at.ac.tuwien.inso.tl.model.Employee;
 import at.ac.tuwien.inso.tl.server.exception.ServiceException;
+import at.ac.tuwien.inso.tl.server.security.AuthenticationSuccessEventListener;
 import at.ac.tuwien.inso.tl.server.service.EmployeeService;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+	private static final Logger LOG = Logger.getLogger(EmployeeServiceImpl.class);
+	
 	@Autowired
 	private EmployeeDao employeeDao;
 	
@@ -28,13 +37,47 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 
 	@Override
-	public int increaseWrongPasswordCounter(String username) {
-		throw new NotYetImplementedException();
+	public int increaseWrongPasswordCounter(String username) throws UsernameNotFoundException {
+		LOG.info("Increasing wrongPasswordCounter by one");
+		
+		List<Employee> employees = employeeDao.findByUsername(username);
+		List<Employee> emp2 = employeeDao.findAll();
+		Employee emp = null;
+		if(employees.isEmpty()){
+			throw new UsernameNotFoundException(String.format("user %s does not exists", username));
+		}
+		
+		if(1 == employees.size()){
+			emp = employees.get(0);
+		} else {
+			throw new UsernameNotFoundException(String.format("Username %s is not unique", username));
+		}
+		
+		emp.setWrongPasswordCounter(emp.getWrongPasswordCounter() + 1);
+		employeeDao.save(emp);
+		LOG.debug(String.format("Increased wrongPasswordCounter to %d", emp.getWrongPasswordCounter()));
+		return emp.getWrongPasswordCounter();
 	}
 
 	@Override
-	public void resetWrongPasswordCounter(String username) {
-		throw new NotYetImplementedException();
+	public void resetWrongPasswordCounter(String username) throws UsernameNotFoundException{
+		LOG.info("Reset wrongPasswordCounter to 0");
+		
+		List<Employee> employees = employeeDao.findByUsername(username);
+		List<Employee> emp2 = employeeDao.findAll();
+		Employee emp = null;
+		if(employees.isEmpty()){
+			throw new UsernameNotFoundException(String.format("user %s does not exists", username));
+		}
+		
+		if(1 == employees.size()){
+			emp = employees.get(0);
+		} else {
+			throw new UsernameNotFoundException(String.format("Username %s is not unique", username));
+		}
+		
+		emp.setWrongPasswordCounter(0);
+		employeeDao.save(emp);
 	}
 
 	// -------------------- For Testing purposes --------------------
