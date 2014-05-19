273e7af8cd9a4d3f7fa4b2f8dc4813940d26adc7
Jakob Kremsner <e1026466@student.tuwien.ac.at>
2014-05-19 10:14:50 +0200

    [ticket #30937] LocationGenerator hinzugefügt + LocationDao + Downmerge

---
+++ tl_db/src/main/java/at/ac/tuwien/inso/tl/dao/LocationDao.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.inso.tl.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import at.ac.tuwien.inso.tl.model.Location;
+
+@Repository
+public interface LocationDao extends JpaRepository<Location, Integer>{
+}
