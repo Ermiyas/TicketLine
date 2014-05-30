791b4f144ccbd255d8cbd60361a28072aee8c219
Robert Bekker <e8325143@student.tuwien.ac.at>
2014-05-30 21:35:59 +0200

    [Bug #33736] Punkte änderbar [Bug #33734] dynamische Fehlermarkierungen (rote Rahmen) und -Meldungen ... und noch ein paar Kleinigkeiten

--- startMvnCleanInstall.cmd
+++ startMvnCleanInstall.cmd
@@ -1,5 +1,8 @@
 @echo off
+setlocal
 call %~dp0\renewEnvironment.cmd
-start "Maven clean" /d "%~dp0\" /wait /min %comspec% /c mvn clean 
-start "Maven install" /d "%~dp0\" /min %comspec% /k mvn install
+pushd %~dp0
+call mvn clean 
+call mvn install
+pause
 
