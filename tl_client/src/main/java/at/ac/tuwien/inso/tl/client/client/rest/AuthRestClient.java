d2d0fe69c3c73025d8294ec4a054813f62c6bcb0
Florian Chlan <florianchlan@punktorg.org>
2014-05-31 15:34:01 +0200

    [ticket #33728] Character-Encoding bei Authentifizierung gefixed

--- tl_client/src/main/java/at/ac/tuwien/inso/tl/client/client/rest/AuthRestClient.java
+++ tl_client/src/main/java/at/ac/tuwien/inso/tl/client/client/rest/AuthRestClient.java
@@ -35,7 +35,7 @@ public class AuthRestClient implements AuthService {
 		RestTemplate restTemplate = this.restClient.getRestTemplate();
 		String url = this.restClient.createServiceUrl("/login");
 		HttpHeaders headers = this.restClient.getHttpHeaders();
-		headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+		headers.add("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
 
 		MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
 		form.add("user", username);
