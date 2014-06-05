dc043a89270972dbd2cbc26b707becaf55d91077
Florian Chlan <florianchlan@punktorg.org>
2014-06-05 12:34:02 +0200

    [ticket #30969] Warenkorb bearbeitet

--- tl_client/src/main/java/at/ac/tuwien/inso/tl/client/gui/controller/ClientShoppingCartController.java
+++ tl_client/src/main/java/at/ac/tuwien/inso/tl/client/gui/controller/ClientShoppingCartController.java
@@ -5,11 +5,15 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import at.ac.tuwien.inso.tl.dto.EntryDto;
 
 @Controller
 @Scope("prototype")
@@ -17,7 +21,19 @@ public class ClientShoppingCartController implements Initializable, ISellTicketS
 	private static final Logger LOG = Logger.getLogger(ClientShoppingCartController.class);
 
 	@FXML
-	private GridPane gpShoppingCart;
+	private TableView<EntryDto> tvCart;
+	@FXML
+	private TableColumn tcCartDescription;
+	@FXML
+	private TableColumn tcCartStatus;
+	@FXML
+	private TableColumn tcCartSinglePrice;
+	@FXML
+	private TableColumn tcCartAmount;
+	@FXML
+	private TableColumn tcCartSum;
+	@FXML
+	private TableColumn tcCartSelection;
 	private ClientSellTicketController parentController;
 	
 	@Override
