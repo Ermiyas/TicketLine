177f12690fc6754461b4a15eb252d5ebf6b51c6a
Jakob Kremsner <e1026466@student.tuwien.ac.at>
2014-06-06 16:59:45 +0200

    [ticket #34246] Kleine Änderung in ReceiptRestClient

--- tl_client/src/main/java/at/ac/tuwien/inso/tl/client/client/rest/ReceiptRestClient.java
+++ tl_client/src/main/java/at/ac/tuwien/inso/tl/client/client/rest/ReceiptRestClient.java
@@ -2,14 +2,24 @@ package at.ac.tuwien.inso.tl.client.client.rest;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import at.ac.tuwien.inso.tl.client.client.ReceiptService;
 import at.ac.tuwien.inso.tl.client.exception.ServiceException;
 import at.ac.tuwien.inso.tl.dto.EntryDto;
 import at.ac.tuwien.inso.tl.dto.PaymentTypeDto;
 import at.ac.tuwien.inso.tl.dto.ReceiptDto;
 
+@Component
 public class ReceiptRestClient implements ReceiptService {
 
+	private static final Logger LOG = Logger.getLogger(ReceiptRestClient.class);
+
+	@Autowired
+	private RestClient restClient;
+	
 	@Override
 	public ReceiptDto createReceiptforEntries(List<EntryDto> enties,PaymentTypeDto pt)  throws ServiceException{
 		//TODO
