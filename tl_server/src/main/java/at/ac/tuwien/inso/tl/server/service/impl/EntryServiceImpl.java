85cdeed6c0eb220a9bfc597fc63391fe4702617e
Greßler Alexander <alexander@gressler.org>
2014-06-07 16:42:19 +0200

    EntryServiceImpl-Dummy damit sich der Server wieder starten lässt.

---
+++ tl_server/src/main/java/at/ac/tuwien/inso/tl/server/service/impl/EntryServiceImpl.java
@@ -0,0 +1,25 @@
+package at.ac.tuwien.inso.tl.server.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import at.ac.tuwien.inso.tl.model.Entry;
+import at.ac.tuwien.inso.tl.server.service.EntryService;
+
+@Service
+public class EntryServiceImpl implements EntryService {
+
+	@Override
+	public Entry createEntry(Entry entry, Integer basket_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<java.util.Map.Entry<Entry, Boolean>> getEntry(Integer basket_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
