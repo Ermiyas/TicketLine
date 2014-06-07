7e93214c5ccbbdb9a7e9b5db062657e3684389b5
Jakob Kremsner <e1026466@student.tuwien.ac.at>
2014-06-07 09:15:01 +0200

    [ticket #34441] TicketDaoImpl und TicketDaoTest erweitert

--- tl_db/src/main/java/at/ac/tuwien/inso/tl/dao/TicketDaoImpl.java
+++ tl_db/src/main/java/at/ac/tuwien/inso/tl/dao/TicketDaoImpl.java
@@ -23,6 +23,13 @@ public class TicketDaoImpl implements TicketDaoCustom {
 			"DELETE FROM entry WHERE ticket_id = :ID";
 	private static final String deleteTicketWithTicketId = 
 			"DELETE FROM ticket WHERE id = :ID";
+	private static final String updateTicketWithShowId =
+			"UPDATE Ticket SET show_id = :show_id WHERE id = :id";
+	private static final String updateEntryWithTicketId =
+			"UPDATE Entry SET ticket_id = :ticket_id WHERE id = :id";
+	private static final String updateSeatWithTicketId =
+			"UPDATE Seat SET ticket_id = :ticket_id WHERE id = :id";
+	
 	
 	@PersistenceContext
 	private EntityManager em;
@@ -30,17 +37,39 @@ public class TicketDaoImpl implements TicketDaoCustom {
 	@Autowired
 	TicketDao tdao;
 	
+	@Autowired
+	EntryDao edao;
+	
 	@Override
 	public Ticket createTicket(Integer show_id, Integer seat_id,
 			Integer entry_id) {
 		LOG.info("createTicket called.");	
 		
-		//LOG.debug("Creating SQL-Statement.");
-		Ticket ticket = null;
+		Ticket t = new Ticket();
+		t = tdao.saveAndFlush(t);
+		
+		if(entry_id != null){
+			Query query = em.createNativeQuery(updateEntryWithTicketId);
+			query.setParameter("ticket_id", t.getId());
+			query.setParameter("id", entry_id);
+			query.executeUpdate();
+		}
+		
+		if(show_id == null && seat_id != null){
+			Query query = em.createNativeQuery(updateSeatWithTicketId);
+			query.setParameter("ticket_id", t.getId());
+			query.setParameter("id", seat_id);
+			query.executeUpdate();
+		}
+		else if(seat_id == null){
+			Query query = em.createNativeQuery(updateTicketWithShowId);
+			query.setParameter("show_id", show_id);
+			query.setParameter("id", t.getId());
+			query.executeUpdate();
+		}
 		
 		
-		
-		return ticket;
+		return t;
 	}
 
 	@Override
--- tl_db/src/test/java/at/ac/tuwien/inso/tl/dao/TicketDaoTest.java
+++ tl_db/src/test/java/at/ac/tuwien/inso/tl/dao/TicketDaoTest.java
@@ -5,8 +5,11 @@ import java.util.Map;
 
 
 
+
+
 import static org.junit.Assert.*;
 
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,19 +18,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import at.ac.tuwien.inso.tl.model.Ticket;
 
 public class TicketDaoTest extends AbstractDaoTest {
+	
+	private static final Logger LOG = Logger.getLogger(TicketDaoTest.class);
+	
 	@Autowired
 	private TicketDao tdao;
 	
 	@Before
-	public void setUp() throws Exception {		
+	public void setUp() throws Exception {
+		LOG.info("setUp called.");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		LOG.info("tearDown called.");
+	}	
+	
+	@Test
+	public void createStandingTicket(){
+		LOG.info("createStandingTicket called.");
+		tdao.createTicket(19, null, null);
+	}
+	
+	@Test
+	public void createSeatTicket(){
+		LOG.info("createSeatTicket called.");
+		tdao.createTicket(null, 50, null);
 	}
 	
 	@Test
 	public void getTicketForEntryWithId1ShouldHaveTicketId1AndBooleanTrueTest(){
+		LOG.info("getTicketForEntryWithId1ShouldHaveTicketId1AndBooleanTrueTest called.");
 		Map.Entry<Ticket, Boolean> entry = tdao.getTicketForEntry(1);
 		assertTrue(entry.getKey().getId() == 1);
 		assertTrue(entry.getValue());
@@ -35,6 +56,7 @@ public class TicketDaoTest extends AbstractDaoTest {
 	
 	@Test
 	public void deleteTicketAndEntryWithTicketIdShouldDeleteTicketAndEntry(){
+		LOG.info("deleteTicketAndEntryWithTicketIdShouldDeleteTicketAndEntry called.");
 		
 		/*
 		assertTrue(tdao.exists(7));
